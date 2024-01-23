16fb509072795bbb624a079df5faba7da645ad34
youssefelaalem <youssefelaalem82@gmail.com>
2024-01-23 18:00:53 +0200

    update the time of expert token

--- utils/generat_token.js
+++ utils/generat_token.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (payloud) => {
   const token = jwt.sign(payloud, process.env.JWT_SECRET_KEY, {
-    expiresIn: "3m",
+    expiresIn: "24h",
   });
   return token;
 };
