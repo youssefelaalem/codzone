c534df9ee24859e52fa7e22a5755a8e39a85c2ef
youssefelaalem <youssefelaalem82@gmail.com>
2024-01-31 23:40:39 +0200

    cc

--- models/User.model.js
+++ models/User.model.js
@@ -31,6 +31,10 @@ const userSchema= new mongoose.Schema({
         type:String,
         enum:[userRoles.ADMIN,userRoles.MANGER,userRoles.USER],
         default:userRoles.USER,
+    },
+    avater:{
+        type:String,
+        default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     }
 })
 module.exports= mongoose.model("User",userSchema)
